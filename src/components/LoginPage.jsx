3b22df79da39f41530db7cc7921869111fc8adaa
karthicbz <pkarthicbz@live.in>
2023-06-25 09:20:29 +0530

    add input fields in login page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,8 +1,18 @@
-import { Text } from "@chakra-ui/react";
+import { Text, Stack, Input, Box, Button } from "@chakra-ui/react";
 
 const LoginPage = ()=>{
     return(
-        <Text fontSize="3xl">Login Page</Text>
+        <Stack spacing={3} alignItems="center" marginTop="100px">
+            <Box w="300px">
+                <Input placeholder="User Id" id="userid" name="userid" type="text"></Input>
+            </Box>
+            <Box w="300px">
+                <Input placeholder="Password" id="password" name="password" type="password"></Input>
+            </Box>
+            <Box w="300px">
+                <Button colorScheme="teal" size="md" width="inherit">Login</Button>
+            </Box>
+        </Stack>
     );
 }
 
